f8f3a44ac386709803df9e5ab69034459e857e77
Nitish Shukla <nitish.shukla@crest.in>
2015-09-10 16:48:46 +0530

    From main method it call the controller and service method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,6 @@
+import com.nitish.controller.UserController;
+import com.nitish.service.UserService;
+
 /**
  * Created by nsm1211 on 10-09-2015.
  */
@@ -5,5 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         // need to the basic initliaztion here
+        new UserController(new UserService());
     }
 }
